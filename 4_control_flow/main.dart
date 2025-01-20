2951fbe7de5f1c19c22c519fe6e30f444ca2407b
Chhai Chivon <chivon.chhai.kh@gmail.com>
2025-01-20 18:06:14 +0700

    Review control flow

--- 4_control_flow/main.dart
+++ 4_control_flow/main.dart
@@ -1,23 +1,33 @@
+import 'dart:io';
 
 void main(){
 
-  // int score = 51;
-  // if(score > 50){
+  // stdout.write("Please input score : ");
+  // int score = int.parse(stdin.readLineSync()!);
+  // if(score >= 50){
   //   print("You are passed");
   // }else{
   //   print("You are failed");
   // } 
   
 
-  // For loop
-  // for (int i = 0 ; i <= 5 ; i++){
-  //   print(i);
-  // }
+//   // For loop
+//   // Increase
+//   for (int i = 0 ; i <= 5 ; i++){
+//     print("Increase Loop i : $i");
+//   }
+//   print("=====================");
+//  // Decrease 
+//   for (int i = 5 ; i >= 0 ; i--){
+//     print("Decrease Loop i : $i");
+//   }
+
+
 
   // While loop
   // int i = 1;
   // while(i <= 5){
-  //     print(i);
+  //     print("While loop i : $i");
   //     i++;
   //     // i+=1;
   //     // i=i+1;
@@ -26,26 +36,27 @@ void main(){
   // Do while loop
   // int i = 1;
   // do {
-  //   print(i);
+  //   print("Do while loop i : $i");
   //   i++;
-  // }while(i <= 5);
+  // }while(i < 5);
 
 
   // Break
-  // for (int i = 0 ; i <= 5 ; i++){
-  //   if(i == 3){
+  // for (int i = 0 ; i <= 10 ; i++){
+  //   // if(i >= 3){
+  //   if(i == 5){
   //     break;
   //   }
-  //   print(i);
+  //   print("Break i : $i");
   // }
 
   // Continue
-  for(int i = 0 ; i <= 5;i++){
-    if(i == 3){
-      continue;
-    }
-    print(i);
-  }
+  // for(int i = 0 ; i <= 5;i++){
+  //   if(i == 3){
+  //     continue;
+  //   }
+  //   print("Continue i : $i");
+  // }
 
 
 
